eea970f2aaf161ce6ae7073f8bcd0c85cb2b737b
marksterben <68014273+marksterben@users.noreply.github.com>
2021-02-09 16:45:19 +0700

    create Header, TodoForm, and Todos component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
+import Header from "./components/Header";
 import Paper from "./components/Paper";
+import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
 import "./styles.css";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn main-black-color">Add</button>
-        <h1 className="header-title">todo list</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn main-black-color">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Belajar Bohong</span>
-        </div>
-
-        <div className="todo">
-          <span className="todo-text">Belajar Nipu</span>
-        </div>
-
-        <div className="todo">
-          <span className="todo-text">Belajar Terus Capek</span>
-        </div>
-      </section>
+      <Header />
+      <TodoForm />
+      <Todos />
     </Paper>
   );
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <section className="todos">
+      <div className="todo">
+        <span className="todo-text">Belajar Bohong</span>
+      </div>
+
+      <div className="todo">
+        <span className="todo-text">Belajar Nipu</span>
+      </div>
+
+      <div className="todo">
+        <span className="todo-text">Belajar Terus Capek</span>
+      </div>
+    </section>
+  );
+};
+
+export default Todos;
