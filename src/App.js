b156b259a86912834289ae86e42029b3a476b842
marksterben <68014273+marksterben@users.noreply.github.com>
2021-02-09 14:56:20 +0700

    add html css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <div className="container">
+              <div className="frame">
+                <section className="header">
+                  <button className="header-btn main-black-color">Add</button>
+                  <h1 className="header-title">todo list</h1>
+                  <button className="header-btn main-red-color">Clear</button>
+                </section>
+
+                <section className="add">
+                  <form className="add-form">
+                    <input type="text" className="add-input" />
+                    <button className="add-btn main-black-color">Add</button>
+                  </form>
+                </section>
+
+                <section className="todos">
+                  <div className="todo">
+                    <span className="todo-text">Belajar Bohong</span>
+                  </div>
+
+                  <div className="todo">
+                    <span className="todo-text">Belajar Nipu</span>
+                  </div>
+
+                  <div className="todo">
+                    <span className="todo-text">Belajar Terus Capek</span>
+                  </div>
+                </section>
+              </div>
+            </div>
   );
 }
