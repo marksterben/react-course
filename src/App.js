80fc1bc2b4542c6811f8c24db500ec17382896bb
marksterben <68014273+marksterben@users.noreply.github.com>
2021-02-09 16:15:09 +0700

    create Paper Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
+import Paper from "./components/Paper";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="container">
-              <div className="frame">
-                <section className="header">
-                  <button className="header-btn main-black-color">Add</button>
-                  <h1 className="header-title">todo list</h1>
-                  <button className="header-btn main-red-color">Clear</button>
-                </section>
+    <Paper>
+      <section className="header">
+        <button className="header-btn main-black-color">Add</button>
+        <h1 className="header-title">todo list</h1>
+        <button className="header-btn main-red-color">Clear</button>
+      </section>
 
-                <section className="add">
-                  <form className="add-form">
-                    <input type="text" className="add-input" />
-                    <button className="add-btn main-black-color">Add</button>
-                  </form>
-                </section>
+      <section className="add">
+        <form className="add-form">
+          <input type="text" className="add-input" />
+          <button className="add-btn main-black-color">Add</button>
+        </form>
+      </section>
 
-                <section className="todos">
-                  <div className="todo">
-                    <span className="todo-text">Belajar Bohong</span>
-                  </div>
+      <section className="todos">
+        <div className="todo">
+          <span className="todo-text">Belajar Bohong</span>
+        </div>
 
-                  <div className="todo">
-                    <span className="todo-text">Belajar Nipu</span>
-                  </div>
+        <div className="todo">
+          <span className="todo-text">Belajar Nipu</span>
+        </div>
 
-                  <div className="todo">
-                    <span className="todo-text">Belajar Terus Capek</span>
-                  </div>
-                </section>
-              </div>
-            </div>
+        <div className="todo">
+          <span className="todo-text">Belajar Terus Capek</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
