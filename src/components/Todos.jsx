879eee191d3a0e56ca62c2b14bc9975c3dcc8405
marksterben <68014273+marksterben@users.noreply.github.com>
2021-03-16 13:03:54 +0700

    initial commit

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -5,17 +5,24 @@ import PropTypes from "prop-types";
 const Todos = ({ todos, completeTodo }) => {
   return (
     <section className="todos">
-      {todos.map((todo, index) => {
-        return (
-          <Todo
-            key={index}
-            text={todo.text}
-            isComplete={todo.isComplete}
-            index={index}
-            completeTodo={completeTodo}
-          />
-        );
-      })}
+      {todos.length > 0 &&
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              text={todo.text}
+              isComplete={todo.isComplete}
+              index={index}
+              completeTodo={completeTodo}
+            />
+          );
+        })}
+      {todos.length === 0 && (
+        <div className="todo-placeholder-text">
+          Click "<span className="add-button-placeholder-text">ADD</span>" to
+          create your todo
+        </div>
+      )}
     </section>
   );
 };
